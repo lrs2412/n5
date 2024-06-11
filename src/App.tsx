cf5d540d29315755687844b48aec88461853276e
Lucas R. Santos <lucas.santos465@fatec.sp.gov.br>
2024-06-10 22:29:59 -0300

    Descrição das alterações

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Heading, Text, Input, Checkbox, Button } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
+    <Box className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Heading as="h1" className="title">
+          Lista de Tarefas
+        </Heading>
+        <Text as="p" className="subtitle">
+          <strong>Inserir nova tarefa</strong>
+        </Text>
+        <Box display="flex" alignItems="center" mb={4}>
+          <Input type="text" className="task-input" placeholder="Digite o título da tarefa" mr={2} marginTop={-18}/>
+          <Checkbox style={{ marginTop: '-18px' }}>Realizada?</Checkbox>
+          <Button colorScheme="red" ml={2} marginTop={-18} color="white" bg="#B91C1C">Inserir</Button>
+        </Box>
+        <Box className="task-container">
+          <Box display="flex" alignItems="center" justifyContent="flex-start" mb={4}>
+            <Text as="p" className="text">Tarefa 1</Text>
+            <Box>
+              <Button colorScheme="red" ml={10} mr={10} color="white" bg="#B91C1C">Pendente</Button>
+              <Button colorScheme="red" color="white" bg="#B91C1C">Excluir</Button>
+            </Box>
+          </Box>
+          <Box display="flex" alignItems="center" justifyContent="flex-start" mb={4}>
+            <Text as="p" className="text">Tarefa 2</Text>
+            <Box>
+              <Button colorScheme="red" ml={10} mr={10} color="white" bg="#B91C1C">Pendente</Button>
+              <Button colorScheme="red" color="white" bg="#B91C1C">Excluir</Button>
+            </Box>
+          </Box>
+          <Box display="flex" alignItems="center" justifyContent="flex-start" mb={4}>
+            <Text as="p" className="text">Tarefa 3</Text>
+            <Box>
+              <Button colorScheme="red" ml={10} mr={10} color="white" bg="#B91C1C">Pendente</Button>
+              <Button colorScheme="red" color="white" bg="#B91C1C">Excluir</Button>
+            </Box>
+          </Box>
+        </Box>
       </header>
-    </div>
+    </Box>
   );
 }
 
